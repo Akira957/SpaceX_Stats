2cd354c96197da1219596129e1653bb0f80a409d
Robert Ramirez <metaknight757@gmail.com>
2019-09-15 18:36:08 -0500

    finished the app

---
+++ public/precache-manifest.ed050ccbe4cdddbcfbad741fbf9205b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f5b662228135f938e481a6a13b16920",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21f5172dcb35b1aff225",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "2d06eb243fa80db7e360",
+    "url": "/static/js/2.7e6e0127.chunk.js"
+  },
+  {
+    "revision": "21f5172dcb35b1aff225",
+    "url": "/static/js/main.95586f00.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
